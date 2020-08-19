7d22a2238289c5c05cc2924fe4950740fe2dc9b3
vrn295 <varun.org295@gmail.com>
2020-08-20 02:38:26 +0530

    'Changed'

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Services.css";
 
 const Services = () => {
-  return <div className="body"></div>;
+  return (
+    <div className="body">
+      <h1>Hello</h1>
+    </div>
+  );
 };
 
 export default Services;

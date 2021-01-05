4cad6b8efc3b6c36afd0f4dbd1f378b3f71f09df
vrn295 <varun.org295@gmail.com>
2021-01-06 05:24:36 +0530

    Updates

---
+++ precache-manifest.2b3852a3ba902953e4926825f53d6d1b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791d609407df44cf13e2197eac5e5a7d",
+    "url": "/trim/index.html"
+  },
+  {
+    "revision": "605782bcd7b389d7b420",
+    "url": "/trim/static/css/main.e56eca60.chunk.css"
+  },
+  {
+    "revision": "786238828e5e689667c3",
+    "url": "/trim/static/js/2.4d8ba2cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trim/static/js/2.4d8ba2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "605782bcd7b389d7b420",
+    "url": "/trim/static/js/main.4d6bb687.chunk.js"
+  },
+  {
+    "revision": "3169b343702dcffaba12",
+    "url": "/trim/static/js/runtime-main.1674b4a6.js"
+  },
+  {
+    "revision": "67d9f0eca6e61a93652ab1919a6db36d",
+    "url": "/trim/static/media/About-1.67d9f0ec.jpg"
+  },
+  {
+    "revision": "11fa7c0f84a770a0163518ea7b038a91",
+    "url": "/trim/static/media/About-2.11fa7c0f.jpg"
+  },
+  {
+    "revision": "ca5376fcd33f3b96b7459a192472c0c8",
+    "url": "/trim/static/media/About-3.ca5376fc.jpg"
+  },
+  {
+    "revision": "49b0d2cf1a6a427652b55c974a1fc73f",
+    "url": "/trim/static/media/About-4.49b0d2cf.jpg"
+  },
+  {
+    "revision": "ec8d0f48ae639d9f4e059c7ae58f05f7",
+    "url": "/trim/static/media/barber-1.ec8d0f48.png"
+  },
+  {
+    "revision": "696e6de6e2e7643146883c1ea141dcd6",
+    "url": "/trim/static/media/barber-2.696e6de6.png"
+  },
+  {
+    "revision": "8e4ea3832126b7271edf116e8765c6a5",
+    "url": "/trim/static/media/gallery1.8e4ea383.jpg"
+  },
+  {
+    "revision": "fd1bf667dd818428605d666c7593c9cb",
+    "url": "/trim/static/media/gallery2.fd1bf667.jpg"
+  },
+  {
+    "revision": "ea84dd06560cdccf03121c7492bec726",
+    "url": "/trim/static/media/gallery3.ea84dd06.jpg"
+  },
+  {
+    "revision": "4b81f0e69b59b9ebb0822f4550a5676e",
+    "url": "/trim/static/media/gallery4.4b81f0e6.jpg"
+  },
+  {
+    "revision": "9b84e0a474e3cda755616127874d6d8e",
+    "url": "/trim/static/media/gallery5.9b84e0a4.jpg"
+  },
+  {
+    "revision": "95646b505e33987e00154aa911fdbde9",
+    "url": "/trim/static/media/gallery6.95646b50.jpg"
+  },
+  {
+    "revision": "67d9f0eca6e61a93652ab1919a6db36d",
+    "url": "/trim/static/media/gallery7.67d9f0ec.jpg"
+  },
+  {
+    "revision": "11fa7c0f84a770a0163518ea7b038a91",
+    "url": "/trim/static/media/gallery8.11fa7c0f.jpg"
+  },
+  {
+    "revision": "ddfef934d5fd53aae64ffbe2807dc4fd",
+    "url": "/trim/static/media/header.ddfef934.jpg"
+  },
+  {
+    "revision": "5a2afae059874d1b12697d88fca98b4f",
+    "url": "/trim/static/media/product1.5a2afae0.jpg"
+  },
+  {
+    "revision": "5a2afae059874d1b12697d88fca98b4f",
+    "url": "/trim/static/media/product2.5a2afae0.jpg"
+  },
+  {
+    "revision": "5a2afae059874d1b12697d88fca98b4f",
+    "url": "/trim/static/media/product3.5a2afae0.jpg"
+  },
+  {
+    "revision": "5a2afae059874d1b12697d88fca98b4f",
+    "url": "/trim/static/media/product4.5a2afae0.jpg"
+  }
+]);

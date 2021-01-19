bc028e03d718653d9db95d35664caa27491ad9a3
vrn295 <varun.org295@gmail.com>
2021-01-20 02:44:54 +0530

    Updates

---
+++ precache-manifest.98c7680df5f3aeba61b7040a7eb2cadf.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbdcf42cc94f7dc6764e4e90fd5b0a9",
+    "url": "/trim/index.html"
+  },
+  {
+    "revision": "0ececeba1cc9639177e8",
+    "url": "/trim/static/css/main.8d5650aa.chunk.css"
+  },
+  {
+    "revision": "33d55866de75231809a0",
+    "url": "/trim/static/js/2.219cf28c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trim/static/js/2.219cf28c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ececeba1cc9639177e8",
+    "url": "/trim/static/js/main.65ed0670.chunk.js"
+  },
+  {
+    "revision": "3169b343702dcffaba12",
+    "url": "/trim/static/js/runtime-main.1674b4a6.js"
+  },
+  {
+    "revision": "67d9f0eca6e61a93652ab1919a6db36d",
+    "url": "/trim/static/media/About-1.67d9f0ec.jpg"
+  },
+  {
+    "revision": "11fa7c0f84a770a0163518ea7b038a91",
+    "url": "/trim/static/media/About-2.11fa7c0f.jpg"
+  },
+  {
+    "revision": "ca5376fcd33f3b96b7459a192472c0c8",
+    "url": "/trim/static/media/About-3.ca5376fc.jpg"
+  },
+  {
+    "revision": "49b0d2cf1a6a427652b55c974a1fc73f",
+    "url": "/trim/static/media/About-4.49b0d2cf.jpg"
+  },
+  {
+    "revision": "78ff58a7995a47f5461af03df21d284f",
+    "url": "/trim/static/media/TRIM-white.78ff58a7.png"
+  },
+  {
+    "revision": "ec8d0f48ae639d9f4e059c7ae58f05f7",
+    "url": "/trim/static/media/barber-1.ec8d0f48.png"
+  },
+  {
+    "revision": "696e6de6e2e7643146883c1ea141dcd6",
+    "url": "/trim/static/media/barber-2.696e6de6.png"
+  },
+  {
+    "revision": "a154545c6e18bd7fb1f70bf795f03f83",
+    "url": "/trim/static/media/facebook.a154545c.png"
+  },
+  {
+    "revision": "8e4ea3832126b7271edf116e8765c6a5",
+    "url": "/trim/static/media/gallery1.8e4ea383.jpg"
+  },
+  {
+    "revision": "fd1bf667dd818428605d666c7593c9cb",
+    "url": "/trim/static/media/gallery2.fd1bf667.jpg"
+  },
+  {
+    "revision": "ea84dd06560cdccf03121c7492bec726",
+    "url": "/trim/static/media/gallery3.ea84dd06.jpg"
+  },
+  {
+    "revision": "4b81f0e69b59b9ebb0822f4550a5676e",
+    "url": "/trim/static/media/gallery4.4b81f0e6.jpg"
+  },
+  {
+    "revision": "9b84e0a474e3cda755616127874d6d8e",
+    "url": "/trim/static/media/gallery5.9b84e0a4.jpg"
+  },
+  {
+    "revision": "95646b505e33987e00154aa911fdbde9",
+    "url": "/trim/static/media/gallery6.95646b50.jpg"
+  },
+  {
+    "revision": "67d9f0eca6e61a93652ab1919a6db36d",
+    "url": "/trim/static/media/gallery7.67d9f0ec.jpg"
+  },
+  {
+    "revision": "11fa7c0f84a770a0163518ea7b038a91",
+    "url": "/trim/static/media/gallery8.11fa7c0f.jpg"
+  },
+  {
+    "revision": "ddfef934d5fd53aae64ffbe2807dc4fd",
+    "url": "/trim/static/media/header.ddfef934.jpg"
+  },
+  {
+    "revision": "07e2ccb09d6ac5cd3116ba1ea94ba72a",
+    "url": "/trim/static/media/instagram.07e2ccb0.png"
+  },
+  {
+    "revision": "8a3ffb631d1295c828d29f101c9998ce",
+    "url": "/trim/static/media/pexels-bri-schneiter-346529.8a3ffb63.jpg"
+  },
+  {
+    "revision": "413bfa77f495941eae8689b61810f42e",
+    "url": "/trim/static/media/pexels-mostafa-meraji-3037244.413bfa77.jpg"
+  },
+  {
+    "revision": "57caa430be2629def3be04fac29c05e5",
+    "url": "/trim/static/media/pngkey.com-beard-png-22689.57caa430.png"
+  },
+  {
+    "revision": "c43e0193b27c536e0abec3536c90a152",
+    "url": "/trim/static/media/product1.c43e0193.png"
+  },
+  {
+    "revision": "e4a18e3dbd2c4ccdbdb28bdf8f2d4e6d",
+    "url": "/trim/static/media/product2.e4a18e3d.png"
+  },
+  {
+    "revision": "fbdbe8d0f7a1636f6d629e2598301d17",
+    "url": "/trim/static/media/product3.fbdbe8d0.png"
+  },
+  {
+    "revision": "be894bd2c0a8adb6dd65812c957325bb",
+    "url": "/trim/static/media/twitter.be894bd2.png"
+  }
+]);
